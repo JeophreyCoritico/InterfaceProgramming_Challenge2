834303502571ccbec29591cc1fcbaadb1324fd6d
JEOPHREY CORITICO <102575814@student.swin.edu.au>
2019-11-18 10:11:32 +1100

    update

--- API/src/app/app.module.ts
+++ API/src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+// import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
+    // LoginComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ References.md
@@ -0,0 +1,4 @@
+https://angular.io/guide/http
+https://stackblitz.com/angular/jykmklgnpnl
+https://angular.io/guide/router
+https://stackblitz.com/angular/aqqyljyojye?file=src%2Fapp%2Fapp.component.html
